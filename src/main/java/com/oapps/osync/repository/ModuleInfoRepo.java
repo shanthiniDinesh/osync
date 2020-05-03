3c942ffe7ba368ff9ce8c39a0d8936af86d5da09
Andrew <andrew@oapps.xyz>
2020-05-03 13:28:05 +0530

    removed 2 files

--- src/main/java/com/oapps/osync/repository/ModuleInfoRepo.java
+++
@@ -1,8 +0,0 @@
-package com.oapps.osync.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.oapps.osync.entity.ModuleInfoEntity;
-
-public interface ModuleInfoRepo extends JpaRepository<ModuleInfoEntity, Long> {
-}
