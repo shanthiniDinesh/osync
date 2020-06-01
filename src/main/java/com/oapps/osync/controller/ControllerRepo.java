c5b035f7871e76dd9d8ec4bb36c971a49198aa8d
Devibalan <devibalan@oapps.xyz>
2020-06-01 13:54:11 +0530

    controll repo changes

--- src/main/java/com/oapps/osync/controller/ControllerRepo.java
+++ src/main/java/com/oapps/osync/controller/ControllerRepo.java
@@ -1,6 +1,8 @@
 package com.oapps.osync.controller;
 
 import com.oapps.osync.controller.freshworks.freshsales.FreshSalesController;
+import com.oapps.osync.controller.outreach.OutreachController;
+import com.oapps.osync.controller.salesLoft.SalesLoftController;
 import com.oapps.osync.controller.zoho.crm.ZohoCRMController;
 
 public class ControllerRepo {
@@ -13,6 +15,12 @@ public class ControllerRepo {
 			break;
 		case "freshsales":
 			controller = new FreshSalesController(moduleName);
+			break;
+		case "salesloft":
+			controller = new SalesLoftController(moduleName);
+			break;
+		case "outreach":
+			controller = new OutreachController(moduleName);
 			break;
 		}
 		return controller;
